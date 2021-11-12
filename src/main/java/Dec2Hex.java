d68693ccd0d45ac71af350a98049a7f529f1c3b2
“Fasteddie830” <montewar8@hotmail.com>
2021-11-12 21:29:23 +0000

    Added comments to the whole process, and also doing the video evidence

--- src/main/java/Dec2Hex.java
+++ src/main/java/Dec2Hex.java
@@ -1,28 +1,28 @@
-import java.util.Scanner;
+import java.util.Scanner; //<--- dis never really used, although it is expected that it will be in future iterations.
 
 class Dec2Hex {
 
 	public static int Arg1;
 
 	public static void main(String args[]) {
-		if(args.length == 0){
+		if(args.length == 0){ //Can't leave the input empty, otherwise error below will show
 			System.out.println("Input was either a 0 or empty, please enter a number above 0");
 			return;
 		}
 		{
 			try {
 				Arg1 = Integer.parseInt(args[0]);
-				if(Arg1 == 0) {
+				if(Arg1 == 0) { //Input can't be a 0 either, since there is no point converting a 0
 					System.out.println("Input was either a 0 or empty, please enter a number above 0");
 					return;
 				}
-				else if(Arg1 <= 0){
+				else if(Arg1 <= 0){ //This sorts out negative numbers. Negative numbers in hex work in funky ways
 					System.out.println("Input was either a 0 or a negative number, please enter a number above 0");
 					return;
 				}
 			}
 
-			catch(Exception e) {
+			catch(Exception e) { //This is for sorting out Strings. We want numbers.
 				System.out.println(args[0] + " is not a valid number, please enter a number above 0");
 				return;
 			}
@@ -33,7 +33,7 @@ class Dec2Hex {
 			String hexadecimal="";
 			System.out.println("Converting the Decimal Value " + num + " to Hex...");
 
-			while(num != 0)
+			while(num != 0)  // to hexadecimal conversion
 			{
 				rem=num%16;
 				hexadecimal= ch[rem] + hexadecimal;
--- src/test/java/Dec2HexTest.java
+++ src/test/java/Dec2HexTest.java
@@ -1,9 +1,9 @@
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test; //import testing framework thing
 
 public class Dec2HexTest {
 
     @Test
-    public void NullTest(){
+    public void NullTest(){ //Testing whatever happens if I don't enter a value code expected to stop with an error
         System.out.println("Testing Start");
         System.out.println("Testing empty value");
         Dec2Hex.main(new String[]{});
@@ -11,28 +11,28 @@ public class Dec2HexTest {
     }
 
     @Test
-    public void StringTest(){
+    public void StringTest(){ //Testing with a string as input, code expected to stop with an error
         System.out.println("Testing string value");
         Dec2Hex.main(new String[]{"Test"});
         System.out.println();
     }
 
     @Test
-    public void PositiveTest(){
+    public void PositiveTest(){ //Testing with a normal integer as input, code is expected to run
         System.out.println("Testing positive int value");
         Dec2Hex.main(new String[]{"4"});
         System.out.println();
     }
 
     @Test
-    public void NegativeTest(){
+    public void NegativeTest(){ //Testing with a negative integer as input, code expected to stop with an error
         System.out.println("Testing negative value");
         Dec2Hex.main(new String[]{"-4"});
         System.out.println();
     }
 
     @Test
-    public void BigTest(){
+    public void BigTest(){  // with a normal bigger integer as input, code is expected to run
         System.out.println("Testing bigger value");
         Dec2Hex.main(new String[]{"10000"});
         System.out.println();
